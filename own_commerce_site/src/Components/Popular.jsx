f3af3c64787294551b67dabec2de06edd8e2e6d8
Kishore Kumar M <142098440+KishoreKumar481@users.noreply.github.com>
2025-07-31 16:03:57 +0530

    Update Popular.jsx

--- own_commerce_site/src/Components/Popular.jsx
+++ own_commerce_site/src/Components/Popular.jsx
@@ -2,10 +2,11 @@ import React, { useEffect, useState } from 'react'
 import Item from './Item.jsx';
 
 export default function Popular() {
+    const url = 'https://shopper-backend-ug5r.onrender.com'
     const [popularProducts, setPopularProducts] = useState([]);
 
     useEffect(() => {
-        fetch('http://localhost:4000/popularinwomen')
+        fetch(`${url}/popularinwomen`)
             .then((res) => res.json())
             .then((data) => setPopularProducts(data))
     }, [])
